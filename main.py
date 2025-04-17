7208c8460ef56bff25a50b95ac6a452e193808ae
Tim Horner <tnactim@gmail.com>
2025-04-16 21:55:53 -0400

    removed hardcoded book and added sys.argv module

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from stats import get_num_words, get_chars_dict, get_sorted_lod
-
+import sys
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
+    #book_path = "books/frankenstein.txt"
     print(f"Analyzing book found at {book_path}")
     text = get_book_text(book_path)
     num_words = get_num_words(text)
@@ -21,6 +25,4 @@ def main():
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-
 main()
